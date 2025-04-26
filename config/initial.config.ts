03036435c6663924722f3485a15ecfd078f6d545
SolsticeLeaf <me@sleaf.dev>
2025-04-26 18:40:27 +0300

    Added more typer texts

--- config/initial.config.ts
+++ config/initial.config.ts
@@ -1,28 +1,32 @@
 export default {
-    nickname: "SolsticeLeaf",
-    siteName: "SLEAF",
-    hero: {
-        avatar_url: 'https://ik.imagekit.io/kiinse/image.svg?updatedAt=1740156640122'
+  nickname: "SolsticeLeaf",
+  siteName: "SLEAF",
+  hero: {
+    avatar_url:
+      "https://ik.imagekit.io/kiinse/image.svg?updatedAt=1740156640122",
+  },
+  icon: "https://ik.imagekit.io/kiinse/icons/icon.svg?updatedAt=1740170186956",
+  socialLinks: [
+    {
+      icon: "pixel:github",
+      url: "https://github.com/SolsticeLeaf",
     },
-    icon: "https://ik.imagekit.io/kiinse/icons/icon.svg?updatedAt=1740170186956",
-    socialLinks: [
-        {
-            icon: 'pixel:github',
-            url: 'https://github.com/SolsticeLeaf'
-        },
-        {
-            icon: 'pixel:linkedin',
-            url: 'https://www.linkedin.com/in/SolsticeLeaf'
-        },
-    ],
-    main_typer: [
-        "Kotlin developer",
-        "Java developer",
-        "Backend developer",
-        "Software engineer"
-    ],
-    announcements: {
-        otherSpeed: 6 as number,
-        desktopSpeed: 8 as number
-    }
-}
+    {
+      icon: "pixel:linkedin",
+      url: "https://www.linkedin.com/in/SolsticeLeaf",
+    },
+  ],
+  main_typer: [
+    "Kotlin developer",
+    "Java developer",
+    "TypeScript developer",
+    "Fullstack developer",
+    "Backend developer",
+    "Frontend developer",
+    "Software engineer",
+  ],
+  announcements: {
+    otherSpeed: 6 as number,
+    desktopSpeed: 8 as number,
+  },
+};
