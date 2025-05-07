ed8d7ea7f11ff67410e1ba85da555822af769b02
SolsticeLeaf <me@sleaf.dev>
2025-05-07 16:39:38 +0300

    Added healthcheck

--- server/api/database/MongoDB.ts
+++ server/api/database/MongoDB.ts
@@ -6,23 +6,25 @@ const dbUrl = process.env.DATABASE_URL || 'mongodb://localhost:27017/test';
 let isConnected = false;
 
 const connectDB = async () => {
-    if (isConnected) { return; }
-    try {
-        await mongoose.connect(dbUrl, {
-            dbName,
-            maxPoolSize: 10
-        });
-        isConnected = true;
-        console.info(`Successfully connected to database ${dbName}!`);
-    } catch (error) {
-        console.error('Error connecting to database: ', error);
-        process.exit(1);
-    }
+  if (isConnected) {
+    return;
+  }
+  try {
+    await mongoose.connect(dbUrl, {
+      dbName,
+      maxPoolSize: 10,
+    });
+    isConnected = true;
+    console.info(`Successfully connected to database ${dbName}!`);
+  } catch (error) {
+    console.error('Error connecting to database: ', error);
+    process.exit(1);
+  }
 };
 
 mongoose.connection.on('disconnected', () => {
-    console.warn('MongoDB disconnected.');
-    isConnected = false;
+  console.warn('MongoDB disconnected.');
+  isConnected = false;
 });
 
-export { connectDB, mongoose };
+export { connectDB, mongoose, isConnected };
--- server/api/healthcheck.ts
+++ server/api/healthcheck.ts
@@ -1,1 +1,30 @@
-export default () => true;
+import pkg from '~/package.json';
+import { connectDB, isConnected } from './database/MongoDB';
+
+const startTime = new Date();
+
+export default defineEventHandler(async () => {
+  try {
+    await connectDB();
+  } catch {}
+  return {
+    status: isConnected ? 'success' : 'fail',
+    uptime: process.uptime(),
+    upSince: startTime,
+    localTime: new Date(),
+    service: {
+      name: pkg.name,
+      version: pkg.version,
+    },
+    database: {
+      mongodb: isConnected,
+    },
+    env: {
+      nodeEnv: process.env.NODE_ENV,
+      nodeVersion: process.version,
+      processName: process.title,
+      pid: process.pid,
+      cwd: process.cwd(),
+    },
+  };
+});
